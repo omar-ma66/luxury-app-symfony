a2c22dd27d6f025ab4e7976cca6c4eb939228050
omar-ma66 <devcodefree66@gmail.com>
2026-06-29 16:42:38 +0200

    on continu

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(UserRepository $userRepository )
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+
+  
+    $alluser= $userRepository->findAll();
+
+    dd($alluser);
+
+
+
+        // return $this->render('profile/index.html.twig', [
+        //     'controller_name' => 'ProfileController',
+        // ]);
     }
 }
